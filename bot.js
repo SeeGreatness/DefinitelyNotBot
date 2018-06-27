6ddd98fd5ce0f20dfcf0676f1354505943491db0
chris laframboise <hire.code.seegreatness@gmail.com>
2018-06-27 09:12:03 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 
-const config = require('config.js');
+//const config = require('config.js'); //this causes errors (if you know where this file is PLEASE let me know!!!
 // config.token contains the bot's token
 //config.prefix contains the bot's prefix
 
