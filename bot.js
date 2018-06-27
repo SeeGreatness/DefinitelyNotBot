557936ade5da45c114a07b66690db4c810d4f273
chris laframboise <hire.code.seegreatness@gmail.com>
2018-06-27 12:24:55 -0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,8 +23,7 @@ client.on("ready", () => {
 // Create an event listener for messages
 client.on("message", (message) => {
   // Exit and stop if it doesn't have the prefix
-  if (!message.content.startsWith(prefix)) return;
-  if (message.content.startsWith(prefix + "ping")) {
+  if (message.content === "ping")) {
     message.channel.send("pong!");
   } else
   if (message.content.startsWith(prefix + "foo")) {
