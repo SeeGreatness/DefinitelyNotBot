f46e591dc2de64b3e5b7c0fde3fce5ab82df3ca9
chris laframboise <hire.code.seegreatness@gmail.com>
2018-08-25 16:09:18 -0600

    Update EvilBot.js

--- EvilBot.js
+++ EvilBot.js
@@ -6,7 +6,6 @@ var botowner = "360894787785719809";
 
 client.on("ready", () => {
     client.user.setStatus("idle");
-    console.log(`Logged in as ${client.user.tag}!`);
     console.log(`${client.user.tag} Is Active!`);
     console.log(`----------------`);
     client.user.setPresence({game: {name: `with you!`, type: 1}});
@@ -20,3 +19,28 @@ client.on('message', msg => {
 });
 
 client.login('process.env.EVILBOT_TOKEN');
+
+
+  // Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.find('name', 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Hey! ${member}, Welcome to the server! right now everyone is like this guy right now--> :fukinpingme: lol`);
+  client.user.setActivity("Playing: your momn!");
+  console.log(`New member joined: ${guild.name} (id: ${guild.id}). This guild now has ${guild.memberCount} members!`);
+
+});
+
+client.on("guildCreate", guild => {
+  // This event triggers when the bot joins a guild.
+  console.log(`New guild joined: ${guild.name} (id: ${guild.id}). This guild has ${guild.memberCount} members!`);
+});
+
+client.on("guildDelete", guild => {
+  // this event triggers when the bot is removed from a guild.
+  console.log(`I have been removed from: ${guild.name} (id: ${guild.id})`);
+});
+
