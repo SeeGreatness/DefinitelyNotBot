bd390dfadd98308c673074ad75a89eb085f19de4
chris laframboise <hire.code.seegreatness@gmail.com>
2018-08-25 16:06:30 -0600

    Update EvilBot.js

--- EvilBot.js
+++ EvilBot.js
@@ -1,8 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const bot = client;
+var prefix = "=";
+var botowner = "360894787785719809";
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+client.on("ready", () => {
+    client.user.setStatus("idle");
+    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`${client.user.tag} Is Active!`);
+    console.log(`----------------`);
+    client.user.setPresence({game: {name: `with you!`, type: 1}});
+    console.log(`Bot has started, with ${client.users.size} users, in ${client.channels.size} channels of ${client.guilds.size} guilds.`);
 });
 
 client.on('message', msg => {
@@ -11,4 +19,4 @@ client.on('message', msg => {
   }
 });
 
-client.login('token');
+client.login('process.env.EVILBOT_TOKEN');
